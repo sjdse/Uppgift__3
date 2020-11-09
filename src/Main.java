0c4bc9c416916cdefe4133ef693b2c517ae29962
Daniel Sollgard Englund <sjdse96@gmail.com>
2020-11-09 13:29:46 +0100

    Several println/printf into single printf

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,12 @@ public class Main {
             Scanner Input = new Scanner(System.in);
             int option = 0;
 
-            System.out.println("\n1. Set user stats\n2. Choose method\n3. Show inputs & start calculation\n4. Exit program");      //Menu output
+            System.out.println("\n1. Set user stats" +          //menu
+                    "\n2. Choose method" +
+                    "\n3. Show inputs & start calculation" +
+                    "\n4. Exit program");
 
-            if (Input.hasNextInt()) {
+            if (Input.hasNextInt()) {                           //menu input
                 option = Input.nextInt();
                 Input.nextLine();     //removes buffered string if there was a invalid input
                 if (option < 1 || option > 4) {
@@ -24,6 +27,7 @@ public class Main {
 
             switch (option) {
                 case 1 -> {
+                    System.out.println(u1.getUserName());
                     System.out.println("What is your username?");
                     u1.setUserName(Input.nextLine());
 
@@ -39,7 +43,7 @@ public class Main {
                     System.out.println("What is your goal experience points?");
                     if (Input.hasNextInt()) {
                         u1.setGoalExp(Input.nextInt());
-                        if (u1.getGoalExp() <= u1.getCurrExp()) {
+                        if (u1.getGoalExp() <= u1.getCurrExp()) {       //exits switch if goalExp is too low
                             break;
                         }
                     } else {
@@ -88,31 +92,34 @@ public class Main {
                 }
                 case 3 -> {
                     if (u1.getGold() >= 0 && i1.getItemSellPrice() >= 0) {   //only runs if the user has gone through case 1 & 2
-                        System.out.println("*** User Info ***");
-                        System.out.println("Username: " + u1.getUserName());
-                        System.out.printf("Current experience: %,d", u1.getCurrExp());
-                        System.out.printf("\nExperience goal: %,d", u1.getGoalExp());
-                        System.out.printf("\nGold: %,d\n", u1.getGold());
-                        System.out.println("---------------------------------------------------");
-                        System.out.println("*** Item Info ***");
-                        System.out.println("Item name: " + i1.getItemName());
-                        System.out.printf("Material cost: %,d", i1.getMatCost());
-                        System.out.printf("\nExperience for crafting: %,d", i1.getItemExp());
-                        System.out.printf("\nItem sell price: %,d\n", i1.getItemSellPrice());
-                        System.out.println("---------------------------------------------------");
+                        System.out.printf("*** User Info ***" +
+                                "\nUsername: %s" +
+                                "\nCurrent experience: %,d" +
+                                "\nExperience goal: %,d" +
+                                "\nGold: %,d" +
+                                "\n---------------------------------------------------" +
+                                "\n*** Item Info ***" +
+                                "\nItem name: %s" +
+                                "\nMaterial cost: %,d" +
+                                "\nExperience for crafting: %,d" +
+                                "\nItem sell price: %,d" +
+                                "\n---------------------------------------------------\n"
+                                , u1.getUserName(), u1.getCurrExp(), u1.getGoalExp(), u1.getGold()
+                                , i1.getItemName(), i1.getMatCost(), i1.getItemExp(), i1.getItemSellPrice());
 
                         int xpToGoal = u1.getGoalExp() - u1.getCurrExp();
                         double craftsToGoal = Math.ceil((double) xpToGoal / i1.getItemExp());  // cast xpToGoal to double to return decimals after division. round up to make sure Exp passes goal
                         double totalCost = craftsToGoal * (i1.getItemSellPrice() - i1.getMatCost());
                         double goldLeft = u1.getGold() + totalCost;
 
-                        System.out.printf("Exp to goal: %,d", xpToGoal);
-                        System.out.printf("\nCrafts to goal: %,.0f\n", craftsToGoal);
-                        System.out.printf("\nCost: %,.0f\t (Positive = profit, Negative = loss)", totalCost);
+                        System.out.printf("Exp to goal: %,d" +
+                                "\nCrafts to goal: %,.0f" +
+                                "\nTotal cost: %,.0f\t (Positive = profit | Negative = loss)", xpToGoal, craftsToGoal, totalCost);
                         if (goldLeft >= 0)
-                            System.out.printf("\nYou will have %,.0f gold after completing your goal.", goldLeft);
+                            System.out.printf("\nYou will have %,.0f gold after completing your goal.\n", goldLeft);
                         else if (goldLeft < 0)
-                            System.out.printf("\nYou need %,.0f gold to complete your goal.", Math.abs(goldLeft));
+                            System.out.printf("\nYou need %,.0f gold to complete your goal.\n", Math.abs(goldLeft));
+
                     } else if (u1.getGold() < 0 || i1.getItemSellPrice() < 0){
                         System.out.println("You have to go through both option 1 and 2 before proceeding to option 3.");
                     }
